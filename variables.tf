762ea22da68b8d1cf13b596b2612cb472d620fe8
John Abiola <rtmabiola@gmail.com>
2023-12-29 13:58:02 +0100

    #3 refactor s3 bucket into the new terrahouse_module

--- variables.tf
+++ variables.tf
@@ -1,9 +1,2 @@
 variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-
-  validation {
-    condition     = length(var.bucket_name) > 3 && length(var.bucket_name) < 64
-    error_message = "Bucket name must be between 4 and 63 characters"
-  }
 }
